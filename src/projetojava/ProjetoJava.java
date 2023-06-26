cc372ba38ba983cb3d7cebfeb67a150869d790ec
Daniel123457 <128873792+Daniel123457@users.noreply.github.com>
2023-06-26 15:54:39 -0300

    abrir

---
+++ src/projetojava/ProjetoJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetojava;
+
+/**
+ *
+ * @author João
+ */
+public class ProjetoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaLogin telalogin = new TelaLogin();
+        telalogin.setVisible(true);
+    }
+    
+}
